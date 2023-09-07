42865998f8737ca52935d3fb25bed988b73d4ac4
Spite <spite-vs@yandex.ru>
2023-09-07 14:12:28 +0300

    first

---
+++ VendingMachine/src/Domen/Product.java
@@ -0,0 +1,33 @@
+package Domen;
+
+public class Product {
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        if (price > 0) {
+            this.price = price;
+        } else {
+            this.price = 10;
+        }
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
